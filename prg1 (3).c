6cb0704d4f3736d804a7be33c059deb7fed6bafc
“deviakhilagit config --global user.email “Akhilagannavarapu@gmail.com <Akhilagannavarapu@gmail.com>
2023-02-21 15:20:33 +0530

    first commit

---
+++ prg1 (3).c
@@ -0,0 +1,22 @@
+#include<stdlib.h> 
+main()
+{
+char str[]="Hello World";
+ char strI[II];
+int i,len; 
+len=strlen(str); 
+for(i=0;i<len;i++)
+{
+strI[i]=str[i]^0; 
+printf("%c",strI[i]);
+}
+printf("\n");
+}
+for(i=O;i<len;i++)
+{
+str3[i] =str2[i]^l27; 
+printf("%c",str3[i]);
+}
+printf("\n");
+}
+
